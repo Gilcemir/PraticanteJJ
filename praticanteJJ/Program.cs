dc52110fdbe3f6eb50d64db69074c8b2a3e7fd6d
Gilcemir <gangelofilho@gmail.com>
2022-01-02 10:10:52 -0300

    adicionando novas funcionalidades

--- praticanteJJ/Program.cs
+++ praticanteJJ/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using praticanteJJ;
+namespace Main_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PraticanteJJ ninja = new PraticanteJJ("Gilcemir Filho");
+            ninja.Rank = "Preta";
+            ninja.Weight = 78;
+            ninja.Age = 30;
+
+
+            ninja.print();
+        }
+
+    }
+}
---
+++ praticanteJJ/praticanteJJ.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praticanteJJ
+{
+    public class PraticanteJJ
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Weight { get; set; }
+        public string Sex { get; set; }
+        public string Rank { get; set; }
+
+        public PraticanteJJ(string name)
+        {
+            Name = name;
+            Rank = "Branca";
+            Weight = 50;
+            Sex = "Masc";
+            Age = 18;
+        }
+
+        public void print()
+        {
+            Console.WriteLine("Nome : {0}", this.Name);
+            Console.WriteLine("Idade : {0}", this.Age);
+            Console.WriteLine("Peso : {0}", this.Weight);
+            Console.WriteLine("Sexo : {0}", this.Sex);
+            Console.WriteLine("Faixa : {0}", this.Rank);
+        }
+    }
+}
+
